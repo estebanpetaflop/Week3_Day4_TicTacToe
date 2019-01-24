7753b32d69cc783b48e7d92ef23907e1bc3ad4f8
JohnnyPepere <jordan.laforet@gmail.com>
2019-01-24 18:46:49 +0100

    almost done

--- lib/application.rb
+++ lib/application.rb
@@ -3,28 +3,26 @@ require 'game.rb'
 class Application
   attr_accessor :name
 
-	def initialize # Setup new game
+	def initialize 
 		@game = Game.new
   end
 
   def perform
 
   puts "Hey, do you wanna play some Tic Tac Toe? Press enter to play"
-    # gets.chomp
-    puts "Alright, bring it on!"
-    puts "Player 1, what’s your name?"
-    print "> "
-    @name1 = gets.chomp
- 
-    puts "Player 2, what’s your name?"
-    print "> "
-    @name2 = gets.chomp
+  puts ">"
+  gets.chomp
+  puts "Alright, bring it on!"
+  puts "Player 1, what’s your name?"
+  print "> "
+  @name1 = gets.chomp
+
+  puts "Player 2, what’s your name?"
+  print "> "
+  @name2 = gets.chomp
   
-    # while true
     current_player=@game.start_game(@name1,@name2)
     @game.turn(current_player)
 
-    # end
-    # Puts the symbol into case
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -5,13 +5,13 @@ require 'verification.rb'
 
 class Game
 
-# attr_accessor :board
+# attr_accessand :board
 
   def initialize
       @@players = []
       @board = Board.new
       @show = Show.new
-      @@usedcases= []
+      @usedcases= []
   end
 
   def start_game (name1,name2)
@@ -25,36 +25,46 @@ class Game
 
   def turn(current_player)
    
-   puts current_player.name + ", time to pick!"
+    puts current_player.name + ", time to pick!"
     puts current_player.name + ", which case do you choose ?"
     print "> "
     boardcase=gets.chomp
-    @@usedcases << boardcase
-    # if @boardcase!= ("A1" || "A2" || "A3" || "B1" || "B2" || "B3" || "C1" || "C2" || "C3") do
-    #     return "Nope, that's not a valid case, try again "
-    #     # puts "> "
-        # @boardcase=gets.chomp
-      # else 
-     
-        # if @@usedcases.include?(boardcase)
-        #   return "Already taken asshole, try another "
+    
 
-      symbol = current_player.symbol
-      board = Board.modify_board(symbol,boardcase)
-      Show.print_board(board)
-      puts "\nCases gagnantes : #{Verification.get_winning_cases(board)}"
-      other_player=@players.reject{|block| block==current_player}[0]
-      turn(other_player)
+
+     if @usedcases.include?(boardcase)
+         puts "Already taken asshole, try another one "
+         print "> "
+          normal_turn(current_player)
+        turn(other_player)
+
+       elsif ["A1","A2","A3","B1","B2","B3","C1","C2","C3"].include?(boardcase)
+         symbol = current_player.symbol
+        board = Board.modify_board(symbol,boardcase)
+        Show.print_board(board)
+        puts "\nCases gagnantes : #{Verification.get_winning_cases(board)}"
+        other_player=@players.reject{|block| block==current_player}[0]
+        @usedcases << boardcase
+        turn(other_player)
+         
+     else puts "Nope, that's not a valid case, read the rules and try again "
+
+         turn(current_player)
+        
+     end
+
+     def normal_turn 
+        boardcase=gets.chomp
+         symbol = current_player.symbol
+        board = Board.modify_board(symbol,boardcase)
+        Show.print_board(board)
+        puts "\nCases gagnantes : #{Verification.get_winning_cases(board)}"
+        other_player=@players.reject{|block| block==current_player}[0]
+        @usedcases << boardcase
+     end
+    
   end
-
-  
-      # changement de player
-  # def change_player(current_player)
-  #   other_player=@@players.delete(current_player)[0]
-  # end
-
-
-  # def end_game
+# def end_game
 
   # end
 
