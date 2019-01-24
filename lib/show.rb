6fb45207e846cd0e5914c335a167ad6f90414c45
cris-dev <cristiano.ferreira.dev@gmail.com>
2019-01-24 20:03:32 +0100

    Optimization of Board Display

--- lib/show.rb
+++ lib/show.rb
@@ -1,22 +1,27 @@
+
 class Show
 
-  
-def self.print_board(board)
+  @@winning_cases = []
+
+  def self.print_board(board)
     system("clear")
     puts
     print_header
     print_separator
 
+    # on détermine s'il y a des cases gagnantes
+    @@winning_cases = Verification.get_winning_cases(board)
+
+    # on affiche le plateau ligne par ligne
     board.each.with_index { |row, i|
-      print_row(row, i)
+      print_row(board, i)
     }
   end
-
 
   private
 
   def self.print_header
-    puts "         A        B        C     "
+    puts "         1        2        3     "
   end
 
   def self.print_separator
@@ -27,27 +32,41 @@ def self.print_board(board)
     puts "     |        |        |        |"
   end
 
-  def self.print_row(row, row_index)
+  def self.print_row(board, i)
     print_space
-    puts "  #{row_index+1}  |   #{get_emoji(row[0])}   |   #{get_emoji(row[1])}   |   #{get_emoji(row[2])}   |"
+    puts "  #{Conversion.row_name(i)}  |   #{get_emoji(board, i, 0)}   |   #{get_emoji(board, i, 1)}   |   #{get_emoji(board, i, 2)}   |"
     print_space
     print_separator
   end
 
-  # détermine l'emoji correspondant au symbole de la case
-  def self.get_emoji(symbol)
-    case symbol
+  # détermine l'emoji correspondant aux coordonnées de la case
+  def self.get_emoji(board, i, j)
+    if is_winning_case(i, j)
+      # emojis gagnants
+      emoji_croix = "\u{2b50}"
+      emoji_rond = "\u{1f642}"
+    else
+      # emojis normaux
+      emoji_croix = "\u{274c}"
+      emoji_rond = "\u{2b55}"
+    end
+
+    case board[i][j] # symbole contenu dans la case
     when "X"
-      return "\u{274c}" # emoji croix
+      return emoji_croix
     when "O"
-      return "\u{2b55}" # emoji rond
+      return emoji_rond
     else
       return "  " # emoji vide
     end
   end
 
-  
-
-  
+  # si on est sur une case gagnante
+  def self.is_winning_case(i, j)
+    # puts "@@winning_cases" + @@winning_cases.to_s
+    # puts "Conversion.case_name(#{i}, #{j})" + Conversion.case_name(i, j)
+    # puts "Test include : " + @@winning_cases.include?(Conversion.case_name(i, j)).to_s
+    return @@winning_cases.include?(Conversion.case_name(i, j))
+  end
 
 end
--- lib/verification.rb
+++ lib/verification.rb
@@ -1,25 +1,22 @@
+
 class Verification
 
+  @@CASE_GROUPS = [
+      ["A1", "A2", "A3"], # cases de la ligne A
+      ["B1", "B2", "B3"], # cases de la ligne B
+      ["C1", "C2", "C3"], # cases de la ligne C
+      ["A1", "B1", "C1"], # cases de la colonne 1
+      ["A2", "B2", "C2"], # cases de la colonne 2
+      ["A3", "A3", "A3"], # cases de la colonne 3
+      ["A1", "B2", "C3"], # cases de la 1ère diagonale
+      ["C1", "B2", "A3"] # cases de la 2ème diagonale
+    ]
 
-  # def self.check_input(boardcase)
-
-  # end
   # vérifie s'il y a un gagnant
   # si oui, retourne le cases gagnantes
   # sinon retourne nil
   def self.get_winning_cases(board)
-    case_groups = [
-      ["A1", "A2", "A3"], # cases de la ligne 1
-      ["B1", "B2", "B3"], # cases de la ligne 2
-      ["C1", "C2", "C3"], # cases de la ligne 3
-      ["A1", "A2", "A3"], # cases de la colonne 1
-      ["A1", "A2", "A3"], # cases de la colonne 2
-      ["A1", "A2", "A3"], # cases de la colonne 3
-      ["A1", "B2", "C3"], # cases de la diagonale 1
-      ["A3", "B2", "C1"] # cases de la diagonale 2
-    ]
-
-    case_groups.each { |group|
+    @@CASE_GROUPS.each { |group|
       if check_cases(board, group)
         return group
       end
@@ -38,39 +35,12 @@ class Verification
       return nil
     end
   end
-# récupere le symbole contenu dans la case
+
+  # récupere le symbole contenu dans la case
   def self.get_case_symbol(board, case_name)
-    x = row_index(case_name)
-    y = col_index(case_name)
-    return board[x][y]
-  end
-
-  # determine l'indice de la ligne
-  def self.row_index(case_name)
-    case case_name[0].upcase
-    when "A"
-      return 0
-    when "B"
-      return 1
-    when "C"
-      return 2
-    else
-      return -1
-    end
-  end
-
-  # détermine l'indice de la colonne
-  def self.col_index(case_name)
-    case case_name[1].upcase
-    when "1"
-      return 0
-    when "2"
-      return 1
-    when "3"
-      return 2
-    else
-      return -1
-    end
+    i = Conversion.row_index(case_name[0])
+    j = Conversion.col_index(case_name[1])
+    return board[i][j]
   end
 
 end
